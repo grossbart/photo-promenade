b639aa4da5e47ce97f16c5e67707d7133fa1951b
Benji <Benji@Artillery.local>
2008-06-26 17:38:58 +0200

    Added styling

--- layouts/error.php
+++ layouts/error.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-
-	<title>Error – <?php echo $params['title']?></title>
-	
-</head>
-
-<body>
-
+	<?php include('elements/header.php'); ?>
 <h1>An error has occured</h1>
 <p>Please return to the <?php echo l('homepage', ''); ?>.</p>
-
-</body>
-</html>
+	<?php include('elements/footer.php'); ?>
--- layouts/index.php
+++ layouts/index.php
@@ -1,30 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-
-	<title><?php echo $params['title']?></title>
-	
-</head>
-
-<body>
-
-<h1><?php echo $params['title']?></h1>
-
-<p><?php echo $params['subtitle']?></p>
-
-<h2>Albums</h2>
-
-<ul>
-<?php
-  foreach(get_folders($params['album_root']) as $album) {
-    echo '<li>'.l($album, $album).'</li>';
-  }
-?>
+	<?php include('elements/header.php'); ?>
+	<h2>Albums</h2>
+<ul class="album_list">
+	<?php
+	foreach(get_folders($params['album_root']) as $album) {
+		echo '<li>'.l($album, $album).'</li>';
+	}
+	?>
+	<br class="clear"/>
 </ul>
-
-
-</body>
-</html>
+	<?php include('elements/footer.php'); ?>
